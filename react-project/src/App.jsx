7cb1584f1e9b8e0e0fe7012191a562df9bf61acb
Ram Sevak Mishra <ramsevakmishra69@gmail.com>
2025-04-28 15:44:53 +0530

    2nd commit

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import SignUp from "./Components/SignUp";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LogIn from "./Components/LogIn";
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route>
-            <Route path="/" element={<SignUp />} />
-          </Route>
-          <Route>
-            <Route path="/login" element={<LogIn />} />
-          </Route>
-        </Routes>
-      </Router>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={<SignUp />} />
+        </Route>
+        <Route>
+          <Route path="/login" element={<LogIn />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
